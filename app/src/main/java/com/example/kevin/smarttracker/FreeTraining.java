d1aa672a4958255669ec3972ee732f4f4fd42485
Kevin Kistner <kevinkistner@gmx.de>
2016-06-22 17:56:08 +0200

    added own list layout

--- app/src/main/java/com/example/kevin/smarttracker/FreeTraining.java
+++ app/src/main/java/com/example/kevin/smarttracker/FreeTraining.java
@@ -2,13 +2,34 @@ package com.example.kevin.smarttracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class FreeTraining extends AppCompatActivity {
+
+    ArrayAdapter<String> adapter;
+    ListView listView;
+    String[] exercises = {"Gefangenen-Kniebeuge", "Liegestütz", "Hüftheben"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_free_training);
 
+
+        listView = (ListView) findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item, exercises);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position) + " is selected", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
