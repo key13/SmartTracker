33d25b1f46defa1bd6a16c46df77b89978513725
IrtazaS <irtaza@MacbookPro-2.local>
2016-07-26 18:47:31 +0200

    Connection

--- app/src/main/java/com/example/kevin/smarttracker/MainActivity.java
+++ app/src/main/java/com/example/kevin/smarttracker/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.kevin.smarttracker;
 
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean mIsBound = false;
+    private ConsumerService mConsumerService = null;
     Button _shortTraining, _fullTraining, _ownTraining, _freeTraining;
+    public static TextView connectionView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        connectionView = (TextView) findViewById(R.id.textView2);
         _shortTraining = (Button) findViewById(R.id.shortTraining);
         _shortTraining.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,5 +72,65 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(myIntent);
             }
         });
+
+        // Bind service
+        mIsBound = bindService(new Intent(MainActivity.this, ConsumerService.class), mConnection, Context.BIND_AUTO_CREATE);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // action with ID action_refresh was selected
+            case R.id.action_refresh:
+                if (mIsBound == true && mConsumerService != null) {
+                    mConsumerService.findPeers();
+                }
+                Toast.makeText(this, "Connect selected", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Clean up connections
+        if (mIsBound == true && mConsumerService != null) {
+            if (mConsumerService.closeConnection() == false) {
+                connectionView.setText("Disconnected");
+            }
+        }
+        // Un-bind service
+        if (mIsBound) {
+            unbindService(mConnection);
+            mIsBound = false;
+        }
+        super.onDestroy();
+    }
+
+    private final ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            mConsumerService = ((ConsumerService.LocalBinder) service).getService();
+            connectionView.setText("onServiceConnected");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName className) {
+            mConsumerService = null;
+            mIsBound = false;
+            connectionView.setText("onServiceDisconnected");
+        }
+    };
 }
